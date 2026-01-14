e6d03d4e55be7a50e3267b0b80ee6fc6e1cf9941
aefff <abdulchoco@yahoo.com>
2026-01-15 02:48:21 +0300

    readded favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
-import { Iceland } from "next/font/google";
+import type {Metadata} from "next";
+import {Iceland} from "next/font/google";
 import Shell from "@/components/Shell";
 import "./globals.css";
 
@@ -11,14 +11,19 @@ const IceLand = Iceland({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "My portfolio",
+    title: "Portfolio",
+    description: "My portfolio",
 };
 
-export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
 
     return (
         <html lang="en">
+        <head>
+            <title>Portfolio</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.png" />
+        </head>
         <body className={`${IceLand.variable} antialiased`}>
         <Shell>
             {children}
